17d153476729c923d1068ae215fd4322d81cd8f4
amandacarvalhomessias-hash <amandacarvalhomessias@gmail.com>
2026-03-01 16:48:55 -0300

    q

--- script.js
+++ script.js
@@ -59,6 +59,18 @@ function mostrarTamanho(select){
 
 }
 
+document.getElementById("comprovante").addEventListener("change", function(){
+
+    const fileName = document.getElementById("file-name")
+
+    if(this.files.length > 0){
+        fileName.textContent = this.files[0].name
+    }else{
+        fileName.textContent = "Nenhum selecionado"
+    }
+
+})
+
 function ativarEventos(){
 
     document.querySelectorAll(".select-abada").forEach(select => {
@@ -86,3 +98,91 @@ function calcularTotal(){
     document.getElementById("valorTotal").innerText = total.toFixed(2);
 
 }
+
+const form = document.getElementById("formConfirmacao");
+form.addEventListener("submit", function(e){
+
+    let pediuAbada = false;
+
+    document.querySelectorAll(".select-abada").forEach(select => {
+
+        if(select.value === "sim"){
+            pediuAbada = true;
+        }
+
+    });
+
+    const arquivo = document.getElementById("comprovante");
+
+    if(pediuAbada && arquivo.files.length === 0){
+
+        e.preventDefault();
+
+        alert("Para confirmar é necessário anexar o comprovante de pagamento.");
+
+    }
+
+});
+
+
+document.getElementById("formConfirmacao").addEventListener("submit", async function(e){
+
+    e.preventDefault()
+    
+    let pessoas = []
+    
+    document.querySelectorAll("#pessoasContainer > div").forEach(pessoa=>{
+    
+    const nome = pessoa.querySelector("input").value
+    const abada = pessoa.querySelector(".select-abada").value
+    const tamanho = pessoa.querySelector(".tamanho-abada select")?.value || ""
+    
+    pessoas.push({
+        nome,
+        abada,
+        tamanho
+    })
+    
+    })
+    
+    const fileInput = document.getElementById("comprovante")
+    
+    let comprovante = null
+    
+    if(fileInput.files.length){
+    
+        const file = fileInput.files[0]
+    
+    const base64 = await new Promise(resolve=>{
+        const reader = new FileReader()
+        reader.onload = ()=> resolve(reader.result.split(",")[1])
+        reader.readAsDataURL(file)
+    })
+    
+    comprovante = {
+        name:file.name,
+        type:file.type,
+        data:base64
+    }
+    
+    }
+    
+    fetch("https://script.google.com/macros/s/AKfycbxE31aoeDKVk4v2U1IjSgB57s1ScDBmGxd6V_AnK5N0_wTCdHFjX_3ra53i3prsGoM/exec",{
+    
+        method:"POST",
+
+        body:JSON.stringify({
+        valor:document.getElementById("valorTotal").innerText,
+        pessoas:pessoas,
+        comprovante:comprovante
+        })
+
+        })
+        .then(r=>r.json())
+        .then(()=>{
+
+        alert("Presença confirmada!")
+
+        })
+
+    })
